d7ea33718fdaa509da4bc9ac1a8a270dcbd5e4f7
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-11-14 00:57:27 -0300

    venda update validator

--- app/Validators/VendaUpdateValidator.ts
+++ app/Validators/VendaUpdateValidator.ts
@@ -9,7 +9,7 @@ export default class VendaUpdateValidator {
       rules.unique({ table: 'compras', column: 'id' }),
       rules.exists({ table: 'compras', column: 'id' }),
     ]),
-    idFuncionario: schema.number.n([
+    idFuncionario: schema.number.nullableAndOptional([
       rules.unique({ table: 'funcionarios', column: 'id' }),
       rules.exists({ table: 'funcionarios', column: 'id' }),
     ]),
@@ -20,5 +20,10 @@ export default class VendaUpdateValidator {
     dataVenda: schema.date.nullableAndOptional({ format: 'yyyy-MM-dd' }),
   })
 
-  public messages: CustomMessages = {}
+  public messages: CustomMessages = {
+    'required': 'o campo {{ field }} é obrigatório',
+    'date.format': '{{ field }} tem que ser formatado como {{ options.format }}',
+    'unique': '{{ field }} tem que ser único',
+    'exists': 'O {{ field }} é obrigatório',
+  }
 }
