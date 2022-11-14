159b346ccea77439a7efcb23d2eb8bbcff5d6afc
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-11-14 00:27:56 -0300

    produto feito

--- app/Validators/ProdutoUpdateValidator.ts
+++ app/Validators/ProdutoUpdateValidator.ts
@@ -1,16 +1,23 @@
 import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class ProdutoValidator {
+export default class ProdutoUpdateValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    nomeFuncionario: schema.string([rules.alpha({ allow: ['space'] }), rules.maxLength(100)]),
+    nome: schema.string.nullableAndOptional([
+      rules.alpha({ allow: ['space'] }),
+      rules.maxLength(100),
+    ]),
 
-    descricao: schema.string([rules.alpha({ allow: ['space'] }), rules.maxLength(700)]),
+    descricao: schema.string.nullableAndOptional([
+      rules.alpha({ allow: ['space'] }),
+      rules.maxLength(100),
+    ]),
 
-    preco: schema.number([rules.range(1, 10000)]),
+    preco: schema.number.nullableAndOptional([rules.range(1, 10000)]),
   })
+
   public messages: CustomMessages = {
     maxLength: 'o número máximo de caractéres do campo {{ field }} é de {{ options.maxLength }}',
     minLength: 'o número máximo de caractéres do campo {{ field }} é de {{ options.minLength }}',
